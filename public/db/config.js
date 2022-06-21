3db0e43533c08bc3d4b11ad149747af255672da5
abstract829 <maxxii.1820@gmail.com>
2022-06-21 18:44:04 -0400

    bd

--- public/db/config.js
+++ public/db/config.js
@@ -8,7 +8,6 @@ const pool = new Pool({
   // port: "5432",
   connectionString:
     "postgresql://postgres:cIO3xuRsA7DF1oy56l4I@containers-us-west-75.railway.app:5471/railway",
-  ssl: { rejectUnauthorized: false },
 });
 
 module.exports = {
