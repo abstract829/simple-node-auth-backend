1128ae3dcc6182700cc2804f594254bc45486eea
abstract829 <maxxii.1820@gmail.com>
2022-06-21 18:45:39 -0400

    fix

--- db/config.js
+++ db/config.js
@@ -10,7 +10,7 @@ const pool = new Pool({
   // database: "dapp-db",
   // port: "5432",
   connectionString:
-    "postgres://vhohafagcjkcyh:b53386fbfbacdcdf9466eacee8a5f987b0ef795f6efc37053e31ce3183d1b404@ec2-3-217-113-25.compute-1.amazonaws.com:5432/d90nnd36aksmpm",
+    "postgresql://postgres:cIO3xuRsA7DF1oy56l4I@containers-us-west-75.railway.app:5471/railway",
   ssl: { rejectUnauthorized: false },
 });
 
